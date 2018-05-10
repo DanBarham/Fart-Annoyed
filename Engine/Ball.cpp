98b1024f6a2838a939a2129e1f67fedcdca03246
Dan Barham <dan.barham@gmail.com>
2018-05-10 15:15:50 -0700

    Added ball update and collision logic

--- Engine/Ball.cpp
+++ Engine/Ball.cpp
@@ -11,3 +11,59 @@ void Ball::Draw(Graphics & gfx) const
 {
 	SpriteCodex::DrawBall( pos,gfx );
 }
+
+void Ball::Update( float dt )
+{
+	pos += vel * dt;
+}
+
+bool Ball::WallCollision(const RectF & walls)
+{
+	bool collided = false;
+	const RectF rect = GetRect();
+
+	//Left / Right
+	if ( rect.left < walls.left )
+	{
+		ReboundX();
+		pos.x += walls.left - rect.left;
+		collided = true;
+	}
+	else if ( rect.right > walls.right )
+	{
+		ReboundX();
+		pos.x -= rect.right - walls.right;
+		collided = true;
+	}
+
+	//Top / Bottom
+	if (rect.top < walls.top)
+	{
+		ReboundY();
+		pos.y += walls.top - rect.top;
+		collided = true;
+	}
+	else if (rect.bottom > walls.bottom)
+	{
+		ReboundY();
+		pos.y -= rect.bottom - walls.bottom;
+		collided = true;
+	}
+
+	return collided;
+}
+
+RectF Ball::GetRect() const
+{
+	return RectF::FromCenter( pos,radius,radius );
+}
+
+void Ball::ReboundX()
+{
+	vel.x *= -1;
+}
+
+void Ball::ReboundY()
+{
+	vel.y *= -1;
+}
--- Engine/Ball.h
+++ Engine/Ball.h
@@ -8,7 +8,12 @@ class Ball
 public:
 	Ball( const Vec2& _pos,const Vec2& _vel );
 	void Draw( Graphics& gfx ) const;
+	void Update( float dt );
+	bool WallCollision( const RectF& walls );
+	RectF GetRect() const;
 private:
+	void ReboundX();
+	void ReboundY();
 	static constexpr float radius = 7.0f;
 	Vec2 pos;
 	Vec2 vel;
